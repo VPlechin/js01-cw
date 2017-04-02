3b2afa9f21f09d303abeea2587ac84002132aca6
Vasylyna <vplechin@yahoo.com>
2017-04-02 11:17:31 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// var greetings = 'Hello, World!!!';
+// alert(greetings);
+// document.write(greetings);
+// console.log(greetings);
+
+// var userName = prompt('Your name:');
+// var greetings = 'Hello ' + userName;
+// document.write(greetings);
+
+// var userAge = Number(prompt('Your age:'));
+// var nextAge = userAge + 1;
+// document.write('Soon you will be ' + nextAge);
+
+// function sum(a, b, c){
+//     var add = a + b + c;
+//     return add;
+// }
+// var num1 = Number(prompt('add first number'));
+// var num2 = Number(prompt('add second number'));
+// var num3 = Number(prompt('add third number'));
+// var result = sum(num1, num2, num3);
+// document.write(result);
